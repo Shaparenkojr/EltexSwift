1a3e4844a38467823f749f0eb862fa19239821ba
shaparenkojr <shaparenkojr@gmail.com>
2024-09-24 19:24:57 +0700

    Initial Commit

---
+++ first_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first_app
+//
+//  Created by Тарас Шапаренко on 24.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
