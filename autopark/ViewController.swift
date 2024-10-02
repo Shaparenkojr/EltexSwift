3ab1494361e1a1365f18aaa0da6c137224589af8
Shaparenkojr <shaparenkojr@gmail.com>
2024-10-02 14:15:58 +0700

    Initial Commit

---
+++ autopark/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autopark
+//
+//  Created by Тарас Шапаренко on 02.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
